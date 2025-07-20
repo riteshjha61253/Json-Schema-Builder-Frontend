cdd3195b167f8888e565717621ae161c257738c4
riteshjha61253 <jha61253@gmail.com>
2025-07-20 22:00:27 +0530

    changes in config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: ['json-schema-builder-frontend.onrender.com'],
+  },
 });
